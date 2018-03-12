425ac1ae7511b31890125f577ac6355b92e87e6c
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-03-12 11:44:27 -0500

    remove title

--- templates/blog_single-loop.php
+++ templates/blog_single-loop.php
@@ -32,7 +32,6 @@ $params = array(
 $_post_format = get_post_format();
 ?>
 <?php
-	print_r($_post_format);
 	switch ($_post_format) {
 		case "video":
 ?>
@@ -239,7 +238,7 @@ $_post_format = get_post_format();
 				<?php } } ?>
 				<div class="post_text">
 					<div class="post_text_inner">
-						<h2 itemprop="name" class="entry_title"><span itemprop="dateCreated" class="date entry_date updated"><?php the_time('d M'); ?><meta itemprop="interactionCount" content="UserComments: <?php echo get_comments_number(qode_get_page_id()); ?>"/></span> <?php the_title(); ?></h2>
+						<h2 itemprop="name" class="entry_title"><span itemprop="dateCreated" class="date entry_date updated"><?php the_time('d M'); ?><meta itemprop="interactionCount" content="UserComments: <?php echo get_comments_number(qode_get_page_id()); ?>"/></span> </h2>
 						<div class="post_info">
 							<span class="time"><?php _e('Posted at','qode'); ?> <?php the_time('H:i'); ?><?php _e('h','qode'); ?></span>
 							<?php _e('in','qode'); ?> <?php the_category(', '); ?>
